d67cb811629d77450488f18bab53101996fd4c95
Sam Vella <svella@vella-it.co.uk>
2015-08-07 22:41:53 +0100

    Final Commit

--- plot3.R
+++ plot3.R
@@ -22,13 +22,26 @@ PowerData <- read.csv.sql("household_power_consumption.txt", sql = "select * fro
 PowerData$DateTime <- strptime(paste(PowerData$Date, " ", PowerData$Time), "%d/%m/%Y %H:%M:%S")
 
 # open the PDF graphics device
-pdf(file="plot1.pdf")
+pdf(file="plot3.pdf")
 
-# Create the plot
+# Create the first plot for sub_metering_1
 # Use type = "l" to create a line
 # ylab sets the verticle label on the left
 # xlab sets the horizontal axis label - which is left blank
-plot(PowerData$DateTime, PowerData$Global_active_power, type="l", ylab="Global Active Power(kilowatts)", xlab="")
+# col sets the line colour - in this case it's being set to black which is normally the default but its nice to make sure!
+with(PowerData, plot(DateTime, Sub_metering_1, type="l", col="black", ylab="Energy sub metering", xlab=""))
+# create the second plot for sub_metering_2 on top of the first using lines()
+# define the colour a red 
+with(PowerData, lines(DateTime, Sub_metering_2, col="red"))
+# And then a third blue plot for sub_metering_3
+with(PowerData, lines(DateTime, Sub_metering_3, col="blue"))
+# set the legend
+# x = topright of the plot (y is null and not required)
+# pch - characters to use - set to NA as we need a line (this could be omitted)
+# col - use a charcter vector to set the colours 
+# Legend - another character vector to set the text for each item
+# lty - define the line thickness
+legend("topright", pch=c(NA,NA,NA), col=c("black", "red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1,1))
 
 # close the graphics device
 dev.off()
